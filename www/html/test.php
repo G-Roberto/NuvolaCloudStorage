8ed5d34fa7e5d3d7bcd5fc83696444dd9805c8d6
Roberto Gentilini <roby.gent@gmail.com>
2023-03-10 15:14:28 +0100

    edited test.php with another approach

--- www/html/test.php
+++ www/html/test.php
@@ -1,32 +1,39 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Upload File</title>
-</head>
-<body>
-  <input type="file" id="file-input" />
-  <script>
-    const uploadFile = async (file) => {
-      const endpoint = 'https://n6pjsuhak0.execute-api.eu-central-1.amazonaws.com/default/upload-item';
-      const filename = file.name;
-      const fileContent = await file.arrayBuffer();
-      const formData = new FormData();
-      formData.append('filename', filename);
-      formData.append('file', new Blob([fileContent]));
+  <head>
+    <meta charset="UTF-8">
+    <title>File Upload</title>
+  </head>
+  <body>
+    <h1>File Upload</h1>
+    <input type="file" id="file-input">
+    <button id="upload-button">Upload</button>
+    <script src="script.js"></script>
+	
+	
+	<script>
+		const uploadFile = async (file) => {
+		  const endpoint = 'https://n6pjsuhak0.execute-api.eu-central-1.amazonaws.com/default/upload-item';
+		  const filename = file.name;
+		  const fileContent = await file.arrayBuffer();
+		  const formData = new FormData();
+		  formData.append('filename', filename);
+		  formData.append('file', new Blob([fileContent]));
 
-      const response = await fetch(endpoint, {
-        method: 'POST',
-        body: formData
-      });
-      const result = await response.text();
-      console.log(result);
-    }
+		  const response = await fetch(endpoint, {
+			method: 'POST',
+			body: formData
+		  });
+		  const result = await response.text();
+		  console.log(result);
+		}
 
-    const fileInput = document.querySelector('#file-input');
-    fileInput.addEventListener('change', async (event) => {
-      const file = event.target.files[0];
-      await uploadFile(file);
-    });
-  </script>
-</body>
+		const fileInput = document.querySelector('#file-input');
+		const uploadButton = document.querySelector('#upload-button');
+		uploadButton.addEventListener('click', async () => {
+		  const file = fileInput.files[0];
+		  await uploadFile(file);
+		});
+	</script>
+  </body>
 </html>
