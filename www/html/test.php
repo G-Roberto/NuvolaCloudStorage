b68f11d4fd41940bcccd6a4233f11555afbe2580
Roberto Gentilini <roby.gent@gmail.com>
2023-03-10 15:31:03 +0100

    edited test.php with yet another approach

--- www/html/test.php
+++ www/html/test.php
@@ -1,39 +1,46 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>File Upload</title>
-  </head>
-  <body>
-    <h1>File Upload</h1>
-    <input type="file" id="file-input">
-    <button id="upload-button">Upload</button>
-    <script src="script.js"></script>
-	
-	
-	<script>
-		const uploadFile = async (file) => {
-		  const endpoint = 'https://n6pjsuhak0.execute-api.eu-central-1.amazonaws.com/default/upload-item';
-		  const filename = file.name;
-		  const fileContent = await file.arrayBuffer();
-		  const formData = new FormData();
-		  formData.append('filename', filename);
-		  formData.append('file', new Blob([fileContent]));
+<input type="file" id="fileInput" />
+<button onclick="uploadFile()">Upload file</button>
 
-		  const response = await fetch(endpoint, {
-			method: 'POST',
-			body: formData
-		  });
-		  const result = await response.text();
-		  console.log(result);
-		}
+<script src="https://sdk.amazonaws.com/js/aws-sdk-2.100.0.min.js"></script>
+<script>
+  function uploadFile() {
+    const file = document.getElementById('fileInput').files[0];
+    const chunkSize = 1024 * 1024; // 1 MB chunks
+    let start = 0;
+    let end = chunkSize;
 
-		const fileInput = document.querySelector('#file-input');
-		const uploadButton = document.querySelector('#upload-button');
-		uploadButton.addEventListener('click', async () => {
-		  const file = fileInput.files[0];
-		  await uploadFile(file);
-		});
-	</script>
-  </body>
-</html>
+    while (start < file.size) {
+      const chunk = file.slice(start, end);
+      const reader = new FileReader();
+
+      reader.onload = function(event) {
+        const chunkData = new Uint8Array(event.target.result);
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://n6pjsuhak0.execute-api.eu-central-1.amazonaws.com/default/upload-item', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              console.log(`Chunk uploaded successfully: ${xhr.responseText}`);
+            } else {
+              console.error(`Error uploading chunk: ${xhr.responseText}`);
+            }
+          }
+        };
+
+        const payload = JSON.stringify({
+          fileName: file.name,
+          fileData: Array.from(chunkData),
+        });
+
+        xhr.send(payload);
+      };
+
+      reader.readAsArrayBuffer(chunk);
+
+      start = end;
+      end = start + chunkSize;
+    }
+  }
+</script>
